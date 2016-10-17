a2ef29b950942a43abec8c8948d3cf363d939d47
unknown <juha-pekka.arimaa@ctrlentertainment.fi>
2016-10-17 18:55:30 +0300

    Added text recognition and updated README

---
+++ vuforia/Assets/Scripts/TextROI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+
+public class TextROI : MonoBehaviour, IVideoBackgroundEventHandler
+{
+	public GUISkin skin;
+	
+	private bool videoBackgroundChanged = true;
+	private Rect trackingRect = new Rect (Screen.width * 0.1f, Screen.height * 0.1f, Screen.width * 0.8f, Screen.height * 0.4f);
+	private Rect detectionRect = new Rect (Screen.width * 0.2f, Screen.height * 0.2f, Screen.width * 0.6f, Screen.height * 0.2f);
+
+	void Update ()
+	{
+		if (videoBackgroundChanged) {
+			Debug.Log ("ROI Added");
+			TextTracker textTracker = (TextTracker)TrackerManager.Instance.GetTracker<TextTracker> ();
+			textTracker.SetRegionOfInterest (detectionRect, trackingRect);
+			videoBackgroundChanged = false;
+		}
+	}
+
+	void OnGUI ()
+	{
+		GUI.skin = skin;
+		GUI.Box (detectionRect, "Detection box");
+	}
+
+	#region IVideoBackgroundEventHandler_IMPLEMENTATION
+
+	// set a flag that the video background has changed. This means the region of interest has to be set again.
+	public void OnVideoBackgroundConfigChanged ()
+	{
+		videoBackgroundChanged = true;
+	}
+
+	#endregion // IVideoBackgroundEventHandler_IMPLEMENTATION
+}
---
+++ vuforia/README.md
@@ -0,0 +1,7 @@
+Pose from image 
+
+![Image recognition](Examples/image_target.png?raw=true "Image recognition")
+
+Pose from text
+
+![Text recognition](Examples/text_recognition.png?raw=true "Text recognition")
