8f3e9a8b95cbe3e4a907be4ca3ed3a1749be3400
unknown <juha-pekka.arimaa@ctrlentertainment.fi>
2016-10-31 21:40:00 +0200

    Added ue_vr, asteroid bp

---
+++ ue_vr/Source/ue_vr/ue_vr.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ue_vr.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue_vr, "ue_vr" );
---
+++ ue_vr/Source/ue_vr/ue_vrGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ue_vrGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_VR_API Aue_vrGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
