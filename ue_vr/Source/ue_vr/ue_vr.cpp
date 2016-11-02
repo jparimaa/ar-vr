0902473b41343bf1518616e96245f3842d46bb9a
unknown <juha-pekka.arimaa@ctrlentertainment.fi>
2016-11-02 20:41:07 +0200

    Removed ue_vr

--- ue_vr/Source/ue_vr/ue_vr.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ue_vr.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue_vr, "ue_vr" );
--- ue_vr/Source/ue_vr/ue_vrGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "ue_vr.h"
-#include "ue_vrGameMode.h"
-
-
-
-
